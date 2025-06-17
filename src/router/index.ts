ce3c7ff17baefe68294e51b51ad6e7d00a7634f7
Moiraine Sedai <chenezu@yahoo.com>
2025-06-17 15:08:49 -0700

    Skeleton packs page

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/packs',
+      name: 'packs',
+      component: () => import('../views/PacksView.vue'),
+    },
   ],
 })
 
---
+++ src/views/packs.ts
@@ -0,0 +1,12 @@
+export const packsData = [
+    {
+        "name": "pack1",
+        "price": 3.99,
+        "pieces": 100
+    },
+    {
+        "name": "pack2",
+        "price": 6.99,
+        "pieces": 200
+    }
+];
