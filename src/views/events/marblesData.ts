c5cb6fab47b70a318c88eb2f4a44e397725a6c45
Moiraine Sedai <chenezu@yahoo.com>
2025-08-01 13:05:46 -0700

    Adding marbles events data

---
+++ src/views/events/marblesData.ts
@@ -0,0 +1,52 @@
+// 97 points per marble
+// estimated costs assumes superior rewards base?
+
+export const marblesRewards = [
+    {level: 1, pointsNeeded: 1000, fragments: 1, otherRewards: "", estMarbles: "11", estCost: "free"},
+    {level: 2, pointsNeeded: 4500, fragments: "1 + 1", otherRewards: "", estMarbles: "47", estCost: "$5 (superior rewards)"},
+    {level: 3, pointsNeeded: 8000, fragments: 1, otherRewards: "", estMarbles: "83", estCost: "$8"},
+    {level: 4, pointsNeeded: 17000, fragments: 2, otherRewards: "1x crown fragment", estMarbles: "176", estCost: "$15"},
+    {level: 5, pointsNeeded: 32000, fragments: 3, otherRewards: "1-300 az", estMarbles: "330", estCost: "$25-$28 (25 + 3)"},
+    {level: 6, pointsNeeded: 60000, fragments: 5, otherRewards: "50 beast elements", estMarbles: "619", estCost: "$63 (60 + 3)"},
+    {level: 7, pointsNeeded: 105000, fragments: 7, otherRewards: "", estMarbles: "1083", estCost: "$118 (90 + 25 + 3)"},
+    {level: 8, pointsNeeded: 165000, fragments: 8, otherRewards: "", estMarbles: "1702", estCost: "$183 (140 + 40 + 3)"},
+    {level: 9, pointsNeeded: 250000, fragments: 9, otherRewards: "", estMarbles: "2578", estCost: "$280 (240 + 40)"},
+    {level: 10, pointsNeeded: 420000, fragments: 10, otherRewards: "", estMarbles: "4329", estCost: "$480 (240x2)"},
+    {level: 11, pointsNeeded: 660000, fragments: 10, otherRewards: "", estMarbles: "6804", estCost: "$745 (240x3 + 25)"},
+
+];
+
+export const marblesPacks = [
+    {level: 1, cost: "$2.99", pieces: 30, dailyCost: "$3", dailySum: 30},
+    {level: 2, cost: "$4.99", pieces: 45, dailyCost: "$8", dailySum: 75},
+    {level: 3, cost: "$6.99", pieces: 63, dailyCost: "$15", dailySum: 138},
+    {level: 4, cost: "$9.99", pieces: 90, dailyCost: "$25", dailySum: 228},
+    {level: 5, cost: "$14.99", pieces: 135, dailyCost: "$40", dailySum: 363},
+    {level: 6, cost: "$19.99", pieces: 180, dailyCost: "$60", dailySum: 543},
+    {level: 7, cost: "$29.99", pieces: 270, dailyCost: "$90", dailySum: 813},
+    {level: 8, cost: "$49.99", pieces: 450, dailyCost: "$140", dailySum: 1263},
+    {level: 9, cost: "$99.99", pieces: 900, dailyCost: "$240", dailySum: 2163},
+];
+
+export const marblesSkinsEstCost = [
+    {level: 1, pieces: 20, totalPieces: 20},
+    {level: 2, pieces: 4, totalPieces: 24},
+    {level: 3, pieces: "4?", totalPieces: 28},
+    {level: 4, pieces: "4?", totalPieces: 32},
+    {level: 5, pieces: "4?", totalPieces: 36},
+    {level: 6, pieces: "4?", totalPieces: 40},
+    {level: 7, pieces: "5?", totalPieces: 45},
+    {level: 8, pieces: "5?", totalPieces: 50},
+    {level: 9, pieces: "5?", totalPieces: 55},
+    {level: 10, pieces: "5?", totalPieces: 60},
+    {level: 11, pieces: "6?", totalPieces: 66},
+    {level: 12, pieces: 7, totalPieces: 73},
+    {level: 13, pieces: 8, totalPieces: 81},
+    {level: 14, pieces: "?", totalPieces: "?"},
+    {level: 15, pieces: "?", totalPieces: "?"},
+];
+
+export const marblesRealData = [
+
+
+];
